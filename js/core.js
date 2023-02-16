881704a3c53c41234533233ba548b91709b77f0f
avel07 <avel.07@mail.ru>
2023-02-15 21:06:14 -0600

    section__list end

--- js/core.js
+++ js/core.js
@@ -1,10 +1,11 @@
 const body = document.body;
-let outsite = false;
 const core = function () {
     return {
         init: () => {
+            // Тут подключаются функции
                 core.Submenu(),
                 core.MobMenu(),
+                // core.FixedHeader(),
                 core.Basket(),
                 core.Personal(),
                 core.Search(),
@@ -16,6 +17,7 @@ const core = function () {
                 core.ReadMore(),
                 core.addFavorite(),
                 core.Filters(),
+                core.Scrolled(),
                 core.Collapse()
         },
         // Показать BackGround
@@ -27,11 +29,13 @@ const core = function () {
             body.classList.remove('popup__show');
         },
         // FixedHeader: () => {
-        //     window.addEventListener("scroll", function () {
-        //         window.scrollY > 300 ? (body.classList.add("")) : (document.querySelector("").classList.remove(""));
-        //     });
-        // },
+        //     header = document.querySelector('header');
 
+        //     window.addEventListener('scroll', core.Throttle(() => {
+        //         window.scrollY > 500 
+
+        //     }, 300))
+        // },
         // Работа с Background при вызове SubMenu
         Submenu: () => {
             let items = Array.from(document.querySelectorAll(".header__submenu")).map(i => i.parentNode);
@@ -48,14 +52,8 @@ const core = function () {
         MobMenu: () => {
             let item__open = document.querySelector('.header__panel__mobile--navbar');
             let item__close = document.querySelector('.header__menu--mob__close');
-            if (core.IsMobile()) {
-                item__open.addEventListener('touchstart', core.MobMenuOpen);
-                item__close.addEventListener('touchstart', core.MobMenuClose);
-            }
-            else {
                 item__open.addEventListener('click', core.MobMenuOpen);
                 item__close.addEventListener('click', core.MobMenuClose);
-            }
         },
         // Открываем мобильное меню
         MobMenuOpen: () => {
@@ -71,14 +69,8 @@ const core = function () {
         Basket: () => {
             let item__open = document.querySelector('.header__panel__basket');
             let item__close = document.querySelector('.header__basket--close');
-            if (core.IsMobile()) {
-                item__open.addEventListener('touchstart', core.BasketOpen);
-                item__close.addEventListener('touchstart', core.BasketClose);
-            }
-            else {
                 item__open.addEventListener('click', core.BasketOpen);
                 item__close.addEventListener('click', core.BasketClose);
-            }
         },
         // Открываем корзину
         BasketOpen: () => {
@@ -94,14 +86,8 @@ const core = function () {
         Personal: () => {
             let item__open = document.querySelector('.header__panel__personal');
             let item__close = document.querySelector('.header__personal--close');
-            if (core.IsMobile()) {
-                item__open.addEventListener('touchstart', core.PersonalOpen);
-                item__close.addEventListener('touchstart', core.PersonalClose);
-            }
-            else {
                 item__open.addEventListener('click', core.PersonalOpen);
                 item__close.addEventListener('click', core.PersonalClose);
-            }
         },
         // Открываем пользователя.
         PersonalOpen: () => {
@@ -117,14 +103,8 @@ const core = function () {
         Search: () => {
             let item__open = document.querySelector('.header__panel__search');
             let item__close = document.querySelector('.header__search--close');
-            if (core.IsMobile()) {
-                item__open.addEventListener('touchstart', core.SearchOpen);
-                item__close.addEventListener('touchstart', core.SearchClose);
-            }
-            else {
                 item__open.addEventListener('click', core.SearchOpen);
                 item__close.addEventListener('click', core.SearchClose);
-            }
         },
         // Открываем поиск
         SearchOpen: () => {
@@ -255,6 +235,7 @@ const core = function () {
                 })
             }
         },
+        //  Умный фильтр в каталоге.
         Filters: () => {
             let items = document.querySelectorAll('.catalog__filter--item__title');
             let open__filter = document.querySelector('.catalog__filter--open');
@@ -287,31 +268,18 @@ const core = function () {
                             }
                         })
                         document.addEventListener('click', (e) => {
-                            if(!e.composedPath().includes(document.querySelector('.catalog__filter--items')))
+                            if(!e.composedPath().includes(document.querySelector('.catalog__filter--items') || !e.composedPath().includes(document.querySelector('.catalog__filter--mob'))))
                             {
                                 for(i=0;i<items.length;i++){
-                                    items[i].nextElementSibling.classList.remove('show');
-                                    items[i].classList.remove('show');
+                                    if(items[i].classList.contains('show') && items[i].nextElementSibling.classList.contains('show')){
+                                        items[i].nextElementSibling.classList.remove('show');
+                                        items[i].classList.remove('show');
+                                    }
                                 }
                             }
                         })
                     }
                 }
-                if(core.IsMobile()){
-                    if(open__filter){
-                        open__filter.addEventListener('touchstart', () =>{
-                            body.classList.add('show__filter');
-                            core.LockScreen();
-                        })
-                    }
-                    if(close__filter){
-                        close__filter.addEventListener('touchstart', () => {
-                            body.classList.remove('show__filter');
-                            core.UnlockScreen();
-                        })
-                    }
-                }
-                else{
                     if(open__filter){
                         open__filter.addEventListener('click', () =>{
                             body.classList.add('show__filter');
@@ -321,14 +289,15 @@ const core = function () {
                     if(close__filter){
                         close__filter.addEventListener('click', () => {
                             for(i=0;i<items.length;i++){
-                                items[i].nextElementSibling.classList.remove('show');
-                                items[i].classList.remove('show');
+                                if(items[i].classList.contains('collapsed') && items[i].nextElementSibling.classList.contains('show')){
+                                    items[i].nextElementSibling.classList.remove('show');
+                                    items[i].classList.remove('collapsed');
+                                }
                             }
                             body.classList.remove('show__filter');
                             core.UnlockScreen();
                         })
                     }
-                }
             },
         // Проверим, мобилка ли
         IsMobile: () => {
@@ -336,6 +305,74 @@ const core = function () {
             (function (a) { if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0, 4))) check = true; })(navigator.userAgent || navigator.vendor || window.opera);
             return check;
         },
+        // Если есть overflow
+        Scrolled: () => {
+        let items = document.querySelectorAll('[data-scrolled]');
+        Element.prototype.isOverflowing = function() {
+            return this.scrollHeight > this.clientHeight || this.scrollWidth > this.clientWidth;
+        }
+        Element.prototype.isScrolled = function() {
+            return this.scrollHeight - this.scrollTop === this.clientHeight
+        }
+        if(items.length > 0){ 
+            let observer = new ResizeObserver(core.Throttle((entries) => {
+                if(entries){
+                for (let entry of entries) {
+                    let position = entry.target.dataset.scrolled;
+                    if(entry.target.scrollHeight > entry.target.clientHeight || entry.target.scrollWidth > entry.target.clientWidth){
+                        entry.target.nextElementSibling.dataset.scroll = position
+                        entry.target.nextElementSibling.setAttribute('data-scroll', position)
+                    }
+                    else{
+                        entry.target.nextElementSibling.dataset.scroll = ''
+                    }
+                }
+            }}, 500));
+            for(i=0;i<items.length;i++){
+                let item = items[i];
+                observer.observe(item);
+                item.addEventListener('scroll', core.Throttle(function(){
+                    let position = item.dataset.scrolled
+                    if (item.isScrolled()) {
+                        item.nextElementSibling.dataset.scroll = ''
+                    }
+                    else{
+                        item.nextElementSibling.dataset.scroll = position
+                    }
+                }, 300))
+            }
+        }
+        },
+        // Игнорирует вызовы передаваемой функции пока не пройдет определенное ожидание
+        Throttle:(func, ms) => {
+            let locked = false
+            return function() {
+              if (locked) return
+              const context = this
+              const args = arguments
+              locked = true
+              setTimeout(() => {
+                func.apply(context, args)
+                locked = false
+              }, ms)
+        
+            }
+          },
+          // Игнорирование вызовов передаваемой функции (resize, scroll, keydown)
+         Debounce: function(func, ms, now) { 
+            let onLast
+            return function() {
+              const context = this
+              const args = arguments 
+              const onFirst = now && !onLast 
+              clearTimeout(onLast)
+              onLast = setTimeout(() => { 
+                onLast = null
+                if (!now) func.apply(context, args) 
+              }, ms)
+              if (onFirst) func.apply(context, args)
+            }
+         },
         // Collapse
         Collapse: () => {
             let togglebuttons = document.querySelectorAll('[data-toggle="collapse"]');
@@ -360,26 +397,9 @@ const core = function () {
         },
     }
 }();
-core.init();
-
-     // Проверяем ширину и высоту внутреннего блока по отношению к родителю
-    Element.prototype.isOverflowing = function() {
-        return this.scrollHeight > this.clientHeight || this.scrollWidth > this.clientWidth;
-    }
-    Element.prototype.isScrolled = function() {
-        return this.scrollHeight - this.scrollTop === this.clientHeight
-    }
 
 document.addEventListener('DOMContentLoaded', () => {
-    let zxc = document.querySelector('.header__basket--body')
-    if(zxc.isOverflowing()){
-        console.log('прокрутка есть')
-    }
-    zxc.addEventListener('scroll', () => {
-        if (zxc.isScrolled()) {
-          console.log('Прокручено до конца')
-        }
-      });
+    core.init();
 })
 
 // HomeSlider
@@ -408,69 +428,7 @@ const homeSection = new Swiper('.catalog__section .swiper', {
       }
 });
 
-
-
-// анимированно скрывает объект
-let _slideUp = (target, duration = 500) => {
-	if (!target.classList.contains('_slide')) {
-		target.classList.add('_slide')
-		target.style.transitionProperty = 'height, margin, padding'
-		target.style.transitionDuration = duration + 'ms'
-		target.style.height = target.offsetHeight + 'px'
-		target.offsetHeight
-		target.style.overflow = 'hidden'
-		target.style.height = 0
-		target.style.paddintTop = 0
-		target.style.paddingBottom = 0
-		target.style.marginTop = 0
-		target.style.marginBottom = 0
-		window.setTimeout(() => {
-			target.hidden = true;
-			target.style.removeProperty('height')
-			target.style.removeProperty('padding-top')
-			target.style.removeProperty('padding-bottom')
-			target.style.removeProperty('margin-top')
-			target.style.removeProperty('margin-bottom')
-			target.style.removeProperty('overflow')
-			target.style.removeProperty('transition-duration')
-			target.style.removeProperty('transition-property')
-			target.classList.remove('_slide')
-		}, duration);
-	}
-}
-
-// анимированно показывает объект
-let _slideDown = (target, duration = 500) => {
-	if (!target.classList.contains('_slide')) {
-		target.classList.add('_slide')
-		if (target.hidden) {
-			target.hidden = false
-		}
-		let height = target.offsetHeight
-		target.style.overflow = 'hidden'
-		target.style.height = 0
-		target.style.paddintTop = 0
-		target.style.paddingBottom = 0
-		target.style.marginTop = 0
-		target.style.marginBottom = 0
-		target.offsetHeight
-		target.style.transitionProperty = 'height, margin, padding'
-		target.style.transitionDuration = duration + 'ms'
-		target.style.height = height + 'px'
-		target.style.removeProperty('padding-top')
-		target.style.removeProperty('padding-bottom')
-		target.style.removeProperty('margin-top')
-		target.style.removeProperty('margin-bottom')
-		window.setTimeout(() => {
-			target.style.removeProperty('height')
-			target.style.removeProperty('overflow')
-			target.style.removeProperty('transition-duration')
-			target.style.removeProperty('transition-property')
-			target.classList.remove('_slide')
-		}, duration)
-	}
-}
-
+const catalogElement = new Swiper('')
 
 // // ScreenLock Для iPhone, но надо потестировать.
 // let screen__lock = false;
